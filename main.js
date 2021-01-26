f71c6afcfcaef9e0eba24b96041ac8ab8b75ac73
saliouvj <m.saliou90@gmail.com>
2021-01-26 10:31:20 +0100

    end

---
+++ main.js
@@ -0,0 +1,19 @@
+// 1.
+let titre = document.getElementsByTagName('h1');
+
+console.log(titre[0]);
+//2.
+let soustitre = document.getElementsByTagName('h3');
+
+console.log(soustitre1[0]);
+console.log(soustitre2[1]);
+console.log(soustitre3[2]);
+
+//3.
+let paragraphe = document.getElementsByTagName('p');
+
+console.log(paragraphe[1]);
+
+let liste = document.getElementsByTagName('li');
+
+console.log(liste[2]);
